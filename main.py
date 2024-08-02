cd0c5f6c1a4c652e9b9566739b4512fa356922f8
mmoua89 <mmoua.89@outlook.com>
2024-08-02 16:31:21 -0700

    Add a check for negative number and display meal cost

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
         print('The meal cost is invalid.')
         return
 
+    if float(meal_cost) < 0:
+        print('Please try again. The meal cost must be a positive number.')
+        return
+
     # Cost calculation
     tax = sale_tax(meal_cost)
     tip = tip_amount(meal_cost)
@@ -19,24 +23,25 @@ def main():
     width = 15
 
     # Display the amounts with formatting
-    print(f'{"Sales tax (7%)":<{width}}: ${tax:.3f}')
-    print(f'{"Tip (18%)":<{width}}: ${tip:.3f}')
+    print(f'{"Meal cost":<{width}}: ${float(meal_cost):.2f}')
+    print(f'{"Sales tax (7%)":<{width}}: ${tax:.2f}')
+    print(f'{"Tip (18%)":<{width}}: ${tip:.2f}')
     print(f'{"Total":<{width}}: ${total:.2f}')
 
 def sale_tax(cost) -> float:
-    '''
+    """
     Calculate the sale tax
     :param cost: the cost
     :return: sale tax amount
-    '''
+    """
     return float(cost) * 0.07
 
 def tip_amount(cost) -> float:
-    '''
+    """
     Calculate the tip amount
     :param cost: the cost
     :return: tip amount
-    '''
+    """
     return float(cost) * 0.18
 
 def valid_number(num) -> bool:
